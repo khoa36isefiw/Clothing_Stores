a856d7111e2011ae7b917c7fe3e4f9da801e8f7c
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-01-16 09:24:53 +0700

    make route and config tailwind

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,4 +1,7 @@
-const publicRoutes = [];
+import DefaultLayout from '../layouts/DefaultLayouts/DefaultLayouts';
+import HomePage from '../pages/HomePage';
+
+const publicRoutes = [{ path: '/', component: HomePage, layout: DefaultLayout }];
 const privateRoutes = [];
 
 export { publicRoutes, privateRoutes };
