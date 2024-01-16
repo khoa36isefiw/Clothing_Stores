2e476ce7b3bbaed780139d21b878b9b0523472b3
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-01-16 09:21:24 +0700

    make adv

---
+++ client/src/layouts/DefaultLayouts/Advertise/Advertise.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Advertise() {
+    return (
+        <div className="bg-[#23856D] h-[50px] text-white font-bold flex justify-center items-center">
+            Follow us and get a chance to win 80% off
+        </div>
+    );
+}
+
+export default Advertise;
